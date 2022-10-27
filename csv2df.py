111006b8414e4f4acfd9380c12f6cb136fe34a53
kenta-hirahara <kenta.hirahara@student.kit.edu>
2022-10-27 21:00:22 +0200

    each substrate plotted in different figure

--- csv2df.py
+++ csv2df.py
@@ -8,15 +8,17 @@ def param_extractor(filepath):
     month = parameter_info[2]
     date = f'{parameter_info[1]}-{month[:3]}-{parameter_info[3]}'
     time = parameter_info[4].replace('_', ':')
-    
     parameter_dict = {}
     
+    print(parameter_list)
+    substrate = re.search('[a-z]+', parameter_list[0], flags=re.IGNORECASE).group()
+    parameter_list = parameter_list[1:]
     for parameter_str in parameter_list:
         value = re.search(r'\d+', parameter_str)
         parameter = re.search('[a-z]+', parameter_str, flags=re.IGNORECASE)
         parameter_dict[parameter.group()] = int(value.group())
     
-    return [parameter_dict, date, time]
+    return [parameter_dict, date, time, substrate]
 
 def E_pump2n(E_pump):
     h, c = 6.626e-34, 3e10
--- photoluminescence.py
+++ photoluminescence.py
@@ -18,6 +18,7 @@ def main():
             path = '/home/kenn/code-python/KSOP/Research-Project-LTI/lab_data'
     files = os.listdir(path)
     files_dir = [f for f in files if os.path.isdir(os.path.join(path, f))]
+    substrates = set()
     for directory in files_dir:
         os.chdir(f'{path}/{directory}')
         path_csv = os.getcwd()
@@ -25,9 +26,13 @@ def main():
         csv_files = [f for f in files if os.path.isfile(os.path.join(path_csv, f)) and '.csv' in f]
         for csv_file in csv_files:
             csv_filepath = f'{path_csv}/{csv_file}'
-            parameter_dict = csv2df.param_extractor(csv_filepath)[0]
-            parameter_dict['Date'] = csv2df.param_extractor(csv_filepath)[1]
-            parameter_dict['Time'] = csv2df.param_extractor(csv_filepath)[2]
+            extracted_params = csv2df.param_extractor(csv_filepath)
+            parameter_dict = extracted_params[0]
+            print(parameter_dict)
+            parameter_dict['Date'] = extracted_params[1]
+            parameter_dict['Time'] = extracted_params[2]
+            parameter_dict['Substrate'] = extracted_params[3]
+            substrates.add(extracted_params[3])
             df_intensity_spectrum = pd.read_csv(csv_filepath, sep='\s+')
             data = df_intensity_spectrum.to_numpy()
             integrated_spectrum = integrate.simps(data[:,1], data[:,0])
@@ -45,39 +50,41 @@ def main():
             # print(s)
             df = pd.concat([df,s])
             # print(param_extractor(csv_filepath))
-    substrate = df.columns[0]
-    df = df.drop(columns='Glass')
-    df = df.sort_values('T')
-    df = df.reindex(columns=['Date', 'Time', 'T', 'A', 'G', 'E', 'E_pump', 'n', 'PLQY'])
-    df.index = [i for i, _ in enumerate(df.index)]
+    # substrate = df.columns[0]
+    # df = df.drop(columns='Silicon')
+    for substrate in substrates:
+        df_substrate = df[df['Substrate'] == substrate]
+        df_substrate = df_substrate.sort_values('T')
+        df_substrate = df_substrate.reindex(columns=['Date', 'Time', 'Substrate', 'T', 'A', 'G', 'E', 'E_pump', 'n', 'PLQY'])
+        df_substrate.index = [i for i, _ in enumerate(df_substrate.index)]
 
-    temp = df['T'].to_list()
-    temp_set = set(temp)
-    sorted_temp_set = sorted(list(temp_set))
+        temp = df_substrate['T'].to_list()
+        temp_set = set(temp)
+        sorted_temp_set = sorted(list(temp_set))
 
-    num_of_fig = len(sorted_temp_set)
-    col = 3 if num_of_fig > 6 else 2
-    row = num_of_fig // col + 1 if num_of_fig % col else num_of_fig // col
-    
-    fig = plt.figure(figsize=(16, 10), dpi=80)
-    fig.suptitle(f'PLQY on {substrate}', fontsize=20)
+        num_of_fig = len(sorted_temp_set)
+        col = 3 if num_of_fig > 6 else 2
+        row = num_of_fig // col + 1 if num_of_fig % col else num_of_fig // col
 
-    for i, temp in enumerate(sorted_temp_set):
-        df_single_temp = df[df['T'] == temp]
-        df_single_temp = df_single_temp.sort_values('n')
+        fig = plt.figure(figsize=(16, 10), dpi=80)
+        fig.suptitle(f'PLQY on {substrate}', fontsize=20)
 
-        n_ndarray = df_single_temp['n'].to_numpy()
-        PLQY_ndarray = df_single_temp['PLQY'].to_numpy()
-    
-        ax = fig.add_subplot(col, row, i+1)
-        # ax.set_xlabel('Carrier density')
-        # ax.set_ylabel(r'PLQY')
-        ax.set_xscale('log')
-        ax.set_yscale('log')
-        ax.set_title(f'{temp}K')
-        ax.set_xlim(4e15, 6e17)
-        ax.scatter(n_ndarray, PLQY_ndarray)
-    plt.show()
+        for i, temp in enumerate(sorted_temp_set):
+            df_single_temp = df_substrate[df_substrate['T'] == temp]
+            df_single_temp = df_single_temp.sort_values('n')
+
+            n_ndarray = df_single_temp['n'].to_numpy()
+            PLQY_ndarray = df_single_temp['PLQY'].to_numpy()
+
+            ax = fig.add_subplot(col, row, i+1)
+            # ax.set_xlabel('Carrier density')
+            # ax.set_ylabel(r'PLQY')
+            ax.set_xscale('log')
+            ax.set_yscale('log')
+            ax.set_title(f'{temp}K')
+            # ax.set_xlim(1e15, 1e18)
+            ax.scatter(n_ndarray, PLQY_ndarray)
+        plt.show()
 
 if __name__ == '__main__':
     main()
