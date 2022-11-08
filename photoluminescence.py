eedb0bf19d76c815c886577e80fc127ac7658041
kenta-hirahara <kenta.hirahara@student.kit.edu>
2022-11-08 07:59:32 +0100

    the functions in csv2df.py got transferred to the main file (photoluminescence.py)

--- photoluminescence.py
+++ photoluminescence.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
+import re
 import os
 import platform
 
 import pandas as pd
 import scipy.integrate as integrate
 from matplotlib import pyplot as plt
-
-import csv2df
 
 def main():
     pd.options.display.precision = 20
@@ -27,13 +26,13 @@ def main():
         for csv_file in csv_files:
             csv_filepath = f'{path_csv}/{csv_file}'
 
-            parameter_dict, date, time, substrate = csv2df.param_extractor(csv_filepath)
+            parameter_dict, date, time, substrate = param_extractor(csv_filepath)
             parameter_dict['Date'] = date
             parameter_dict['Time'] = time
             parameter_dict['Substrate'] = substrate
 
             substrates.add(substrate)
-            
+
             df_intensity_spectrum = pd.read_csv(csv_filepath, sep='\s+')
             data = df_intensity_spectrum.to_numpy()
             integrated_spectrum = integrate.simps(data[:,1], data[:,0])
@@ -41,7 +40,7 @@ def main():
             V = parameter_dict['E'] / 1000
             E_pump = - 1e-8*V**4 + 4e-8*V**3 + 8e-9*V**2 + 1e-9*V  + 4e-10
             parameter_dict['E_pump'] = E_pump
-            n = csv2df.E_pump2n(E_pump)
+            n = E_pump2n(E_pump)
             parameter_dict['n'] = n
             PLQY = PL / n
             parameter_dict['PLQY'] = PLQY
@@ -81,6 +80,34 @@ def main():
             # ax.set_xlim(1e15, 1e18)
             ax.scatter(n_ndarray, PLQY_ndarray)
     plt.show()
+
+def param_extractor(filepath: str):
+    split_path = filepath.split('/')
+    filename = split_path[-1]
+    parameter_info = filename.split(' ')
+    parameter_list = parameter_info[0].split('_')
+    month = parameter_info[2]
+    date = f'{parameter_info[1]}-{month[:3]}-{parameter_info[3]}'
+    time = parameter_info[4].replace('_', ':')
+    parameter_dict = {}
+
+    # print(parameter_list)
+    substrate = re.search('[a-z]+', parameter_list[0], flags=re.IGNORECASE).group()
+    parameter_list = parameter_list[1:]
+    for parameter_str in parameter_list:
+        value = re.search(r'\d+', parameter_str)
+        parameter = re.search('[a-z]+', parameter_str, flags=re.IGNORECASE)
+        parameter_dict[parameter.group()] = int(value.group())
+
+    return parameter_dict, date, time, substrate
+
+def E_pump2n(E_pump):
+    h, c = 6.626e-34, 3e10
+    λ = 5.32e-5
+    d, spotsize = 2.3e-5, 7.8e-3
+    abs = 7.3e-1
+
+    return E_pump * λ / h / c * abs / spotsize / d
 
 if __name__ == '__main__':
     main()
